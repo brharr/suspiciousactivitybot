d09c5607393dc380024ef46000b1a2b56de0fb39
brharr <brharr@microsoft.com>
2017-04-07 15:35:01 -0400

    Building out the required REST Post call.

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var builder = require('botbuilder');
 var Client = require('node-rest-client').Client;
 
-var client = new Client();
-client.registerMethod('jsonMethod', 'http://suspicious-incident-api.azurewebsites.net/api/')
-
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 bot.dialog('/', [
@@ -17,39 +14,68 @@ bot.dialog('/', [
             builder.Prompts.text(session, 'Please describe the suspicious activity.');
     },
     function (session, results) {
-        session.dialogData.activity = results.response;
+        session.privateConversationData.activity = results.response;
         builder.Prompts.text(session, 'Is there any other information that you would like to add before I ask for vehicle and location information?');
     },
     function (session, results) {
         if (results.response.toLowerCase() == 'no')
             builder.Prompts.text(session, 'Please describe any vehicle that may have been involved in the suspicious activity.')
         else {
-            session.userData.activity.concat(results.response);
+            session.privateConversationData.activity.concat(results.response);
             builder.Prompts.text(session, 'Please describe any vehicle that may have been involved in the suspicious activity.')
         }
     },
     function (session, results) {
-        session.dialogData.vehicle = results.response;
+        session.privateConversationData.vehicle = results.response;
         builder.Prompts.text(session, 'Please provide any physical identifiers of anyone you observed.');
     },
     function (session, results) {
-        session.dialogData.people = results.response;
+        session.privateConversationData.people = results.response;
         builder.Prompts.text(session, 'Please provide information about the location where the suspicious activity was seen or heard.');
     },
     function (session, results) {
-        session.dialogData.fromLocation = results.response;
+        session.privateConversationData.fromLocation = results.response;
         builder.Prompts.text(session, 'Please provide any information about the location where the people involved may have gone.');
     },
     function (session, results) {
-        session.dialogData.toLocation = results.response;
+        session.privateConversationData.toLocation = results.response;
         builder.Prompts.text(session, 'Would you please provide your contact information should we have any more questions?');
     },
     function (session, results) {
-        if (results.response.toLowerCase() == 'no')
-            session.endConversation('Thank you for providing us with this suspicious activity information.');
-        else{
-            session.dialogData.contact = results.response;
+        if (results.response.toLowerCase() == 'no') {
+            session.send('Validating Session Data: ', session.privateConversationData.activity);
+            sendActivity(session);
+            session.endConversation('Thank you for providing us with this suspicious activity information.');s
+        }
+        else {
+            session.privateConversationData.contact = results.response;
+            session.send('Validating Session Data: ', session.privateConversationData.activity);
+            sendActivity(session);
             session.endConversation('Thank you for providing us with this suspicious activity information.');
         }
     }
 ]);
+
+function sendActivity(session) {
+    var client = new Client();
+    client.registerMethod('jsonMethod', 'http://suspicious-incident-api.azurewebsites.net/api/incidents', 'POST');
+
+    // set content-type header and data as json in args parameter 
+    var args = {
+        data: { 
+            "incidentdesc" : session.privateConversationData.activity,
+            "vehicle" : session.privateConversationData.vehicle,
+            "fromLocation" : session.privateConversationData.fromLocation,
+            "toLocation" : session.privateConversationData.toLocation,
+            "name": session.privateConversationData.contact
+        },
+        headers: { "Content-Type": "application/json" }
+    };
+
+    client.methods.postMethod(args, function (data, response) {
+        // parsed response body as js object 
+        console.log(data);
+        // raw response 
+        console.log(response);
+    });
+}
